65ef0b3e85404585a7da18a2149854f6c224bba1
Barbiche <guethomas3@gmail.com>
2016-04-29 09:03:22 +0200

    Ajout Créer Parcours

---
+++ app/src/main/java/boulhexanome/application_smartooz/CreerParcours_AjoutTag.java
@@ -0,0 +1,13 @@
+package boulhexanome.application_smartooz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreerParcours_AjoutTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creer_parcours__ajout_tag);
+    }
+}
