c4504f2037e9679382072d51ce103cf5dda9d360
Lisa Courant <lisa.courant@gmail.com>
2016-04-29 08:53:42 +0200

    recherche de parcours

--- app/src/main/java/boulhexanome/application_smartooz/VisiterLyonActivity.java
+++ app/src/main/java/boulhexanome/application_smartooz/VisiterLyonActivity.java
@@ -22,7 +22,7 @@ public class VisiterLyonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visiterLyon);
+        setContentView(R.layout.activity_visiter_lyon);
         final EditText edittext = (EditText)findViewById(R.id.rech_editText);
         assert edittext != null;
 
