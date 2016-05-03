07d18e4a2ef702b1003ae249795cd7c222f0631a
HugoDelval <hugo.delval@insa-lyon.fr>
2016-05-03 10:50:27 +0200

    upload image à tester.. beaucoup

--- app/src/main/java/boulhexanome/application_smartooz/Activities/CircuitDetailsActivity.java
+++ app/src/main/java/boulhexanome/application_smartooz/Activities/CircuitDetailsActivity.java
@@ -1,9 +1,15 @@
 package boulhexanome.application_smartooz.Activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +17,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import org.apache.commons.codec.binary.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,13 +26,16 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.internal.http.multipart.MultipartEntity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -39,10 +50,22 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import boulhexanome.application_smartooz.Model.Circuit;
@@ -66,6 +89,7 @@ public class CircuitDetailsActivity extends AppCompatActivity implements OnMapRe
     private MapFragment mMapFragment;
     private GoogleMap mMap;
     private int numberOfReceivedPlaces;
+    private Uri fileUri;
 
     // Interface
     private ActionBar toolbar;
@@ -82,6 +106,11 @@ public class CircuitDetailsActivity extends AppCompatActivity implements OnMapRe
     private Circuit theCircuit;
     private ArrayList<Place> listOfPlaces = new ArrayList<Place>();
     private boolean parcoursEstLance = false;
+    private Uri selectedImage;
+    private Bitmap photo;
+    private String picturePath;
+    private String ba1;
+    private String url;
 
 
     @Override
@@ -167,29 +196,41 @@ public class CircuitDetailsActivity extends AppCompatActivity implements OnMapRe
             });
 
             final FloatingActionButton add = (FloatingActionButton) findViewById(R.id.action_add_photo);
+            assert add != null;
             add.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
-                    int TAKE_PHOTO_CODE = 0;
-                    String file = "hola.jpg";
-                    File newfile = new File(file);
-                    try {
-                        newfile.createNewFile();
+//                    int TAKE_PHOTO_CODE = 0;
+//                    String file = "hola.jpg";
+//                    File newfile = new File(file);
+//                    try {
+//                        newfile.createNewFile();
+//                    }
+//                    catch (IOException e)
+//                    {
+//                    }
+//                    Uri outputFileUri = Uri.fromFile(newfile);
+//                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+//                    startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+
+                    if (getApplicationContext().getPackageManager().hasSystemFeature(
+                            PackageManager.FEATURE_CAMERA)) {
+                        // Open default camera
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                        // start the image capture Intent
+                        startActivityForResult(intent, 100);
+
+                    } else {
+                        Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
                     }
-                    catch (IOException e)
-                    {
-                    }
-                    Uri outputFileUri = Uri.fromFile(newfile);
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
 
-                    startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+                    url = Config.getRequest(Config.UPLOAD_PICTURE_PLACE_CIRCUIT + "/" + Integer.toString(theCircuit.getId()));
 
-                    String url = Config.getRequest(Config.UPLOAD_PICTURE_PLACE_CIRCUIT + "/" + Integer.toString(theCircuit.getId()));
-
-                    PostTask postTask = new PostTask(url);
-                    postTask.delegate = new HandleVisualization(CircuitDetailsActivity.this);
                     //postTask.execute(newfile);
                 }
             });
@@ -200,34 +241,6 @@ public class CircuitDetailsActivity extends AppCompatActivity implements OnMapRe
         mScrollView.setVisibility(View.GONE);
 
     } // Fin onCreate
-
-    class HandleVisualization implements PostTask.AsyncResponse{
-
-        private CircuitDetailsActivity circuitDetailsActivity;
-
-        public HandleVisualization(CircuitDetailsActivity circuitDetailsActivity) {
-            this.circuitDetailsActivity = circuitDetailsActivity;
-        }
-
-        @Override
-        public void processFinish(JsonObject results) {
-            this.circuitDetailsActivity.visualizeReceived(results);
-        }
-    }
-
-    class HandleGetPlaces implements GetTask.AsyncResponse{
-
-        private CreerParcours creerParcours;
-
-        public HandleGetPlaces(CreerParcours creerParcours) {
-            this.creerParcours = creerParcours;
-        }
-
-        @Override
-        public void processFinish(JsonObject results) {
-            this.creerParcours.getPlacesReceived(results);
-        }
-    }
 
 
     public void clickLancerParcours(){
@@ -525,6 +538,42 @@ public class CircuitDetailsActivity extends AppCompatActivity implements OnMapRe
         }
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+
+            selectedImage = data.getData();
+            photo = (Bitmap) data.getExtras().get("data");
+
+            // Cursor to get image uri to display
+
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//            ImageView imageView = (ImageView) findViewById(R.id.Imageprev);
+//            imageView.setImageBitmap(photo);
+
+
+            Bitmap bm = BitmapFactory.decodeFile(picturePath);
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+            byte[] ba = bao.toByteArray();
+            ba1 = Arrays.toString(Base64.encodeBase64(ba));
+
+            Log.e("base64", "-----" + ba1);
+
+            // Upload image to server
+            new UploadToServer(this, url, ba1).execute();
+
+        }
+    }
+
 
 }
 
@@ -557,3 +606,47 @@ class HandleGetPlacesDetailsCircuit implements GetTask.AsyncResponse {
 }
 
 
+class UploadToServer extends AsyncTask<Void, Void, String> {
+
+    private final String url;
+    private final String base64;
+    private ProgressDialog pd;
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd.setMessage("Wait image uploading!");
+        pd.show();
+    }
+
+    public UploadToServer(CircuitDetailsActivity circuitDetailsActivity, String url, String base64){
+        pd = new ProgressDialog(circuitDetailsActivity);
+        this.url = url;
+        this.base64 = base64;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("base64", base64));
+        nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            String st = EntityUtils.toString(response.getEntity());
+            Log.v("log_tag", "In the try Loop" + st);
+
+        } catch (Exception e) {
+            Log.v("log_tag", "Error in http connection " + e.toString());
+        }
+        return "Success";
+
+    }
+
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        pd.hide();
+        pd.dismiss();
+    }
+}
